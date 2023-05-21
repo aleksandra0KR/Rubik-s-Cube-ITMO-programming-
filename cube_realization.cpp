dbd471d589894c43a8e44efc8a77896bffd6946a
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2023-05-21 23:23:48 +0300

    Update cube_realization.cpp
    randown

--- cube_realization.cpp
+++ cube_realization.cpp
@@ -48,6 +48,52 @@ colors return_clor(char col){
     }
 }
 
+void Cube::random_situation() {
+    int number_of_flips = rand() % 12;
+    for(int i = 0; i < number_of_flips; i++){
+        int command = rand() % 12;
+        switch(command){
+            case 0 :
+                this->right_rotation_90();
+                break;
+            case 1:
+                this->right_rotation_against_90();
+                break;
+            case 2:
+                this->left_rotation_90();
+                break;
+            case 3:
+                this->left_rotation_against_90();
+                break;
+            case 4:
+                this->up_rotation_90();
+                break;
+            case 5:
+                this->up_rotation_against_90();
+                break;
+            case 6:
+                this->down_rotation_90();
+                break;
+            case 7:
+                this->down_rotation_against_90();
+                break;
+            case 8:
+                this->front_rotation_90();
+                break;
+            case 9:
+                this->front_rotation_against_90();
+                break;
+            case 10:
+                this->back_rotation_90();
+                break;
+            case 11:
+                this->back_rotation_against_90();
+                break;
+        }
+        this->show_to_concol();
+    }
+}
+
 void Cube::fill_from_the_file(const char* name_of_the_file) {
     ifstream file;
     file.open(name_of_the_file);
@@ -222,62 +268,145 @@ bool Cube::has_all_colors() {
 }
 
 bool Cube::check(){
-    bool WY = down[1][1] == WHITE and up[1][1] == YELLOW;
-    bool BG = left[1][1] == BLUE and right[1][1] == GREEN;
-    bool RO = front[1][1] == RED and back[1][1] == ORANGE;
+    bool WY = down[1][1] == RED and up[1][1] == ORANGE;
+    bool BG = left[1][1] == GREEN and right[1][1] == BLUE;
+    bool RO = front[1][1] == WHITE and back[1][1] == YELLOW;
     return WY and BG and RO and has_all_colors();
 }
 
 
 void Cube::show() {
     ofstream out("Cube.out");
-    out << endl;
-    for (int i = 0; i < 3; i++) {
-        out << "    ";
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(up[i][j]);
+    for (int i = 0; i < size; i++) {
+        out << "       ";
+        for (auto col: up[i]) {
+            out << return_letter(col) << " ";
         }
         out << endl;
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(left[i][j]);
+    for (int i = 0; i < size; i++) {
+        for (auto col: left[i]) {
+            out << return_letter(col) << " ";
         }
 
         out << " ";
 
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(front[i][j]);
+        for (auto col: front[i]) {
+            out << return_letter(col) << " ";
         }
 
         out << " ";
 
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(right[i][j]);
+        for (auto col: right[i]) {
+            out << return_letter(col) << " ";
         }            out << " ";
 
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(right[i][j]);
-        }
-
-        out << " ";
-
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(back[i][j]);
+        for (auto col: back[i]) {
+            out << return_letter(col) << " ";
         }
 
         out << endl;
     }
 
-    for (int i = 0; i < 3; i++) {
-        out << "    ";
-        for (int j = 0; j < 3; j++) {
-            out << return_letter(down[i][j]);
+    for (int i = 0; i < size; i++) {
+        out << "       ";
+        for (auto col: down[i]) {
+            out << return_letter(col) << " ";
         }
         out << endl;
     }
 
     out << endl;
     out.close();
+}
+
+
+void Cube::show_to_concol() {
+    cout << "This is your cube right now: " << '\n';
+    for (int i = 0; i < size; i++) {
+        cout << "       ";
+        for (auto col: up[i]) {
+            cout << return_letter(col) << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (auto col: left[i]) {
+            cout << return_letter(col) << " ";
+        }
+
+        cout << " ";
+
+        for (auto col: front[i]) {
+            cout << return_letter(col) << " ";
+        }
+
+        cout << " ";
+
+        for (auto col: right[i]) {
+            cout << return_letter(col) << " ";
+        }            cout << " ";
+
+        for (auto col: back[i]) {
+            cout << return_letter(col) << " ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << "       ";
+        for (auto col: down[i]) {
+            cout << return_letter(col) << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    /*
+    for(int i = 0; i < size; i++){
+       for (auto col: front[i]){
+            cout<<return_letter(col)<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i = 0; i < size; i++){
+        for (auto col: back[i]){
+            cout<<return_letter(col)<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i = 0; i < size; i++){
+        for (auto col: left[i]){
+            cout<<return_letter(col)<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i = 0; i < size; i++){
+        for (auto col: right[i]){
+            cout<<return_letter(col)<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i = 0; i < size; i++){
+        for (auto col: up[i]){
+            cout<<return_letter(col)<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i = 0; i < size; i++){
+        for (auto col: down[i]){
+            cout<<return_letter(col)<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+*/
 }
