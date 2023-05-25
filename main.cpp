087061a24af295e3fa046c7d7b28acfdb94349f5
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2023-05-25 20:38:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
 #include "cube.h"
 
-int main(){
+int main() {
+
 
     Cube my_cube;
-    //cout << my_cube.check() << '\n';
-    //my_cube.fill_from_the_file("file.txt");
-    //cout <<my_cube.has_all_colors() << '\n';
 
-   // my_cube.right_rotation_90();
-    //my_cube.right_rotation_against_90();
-    //my_cube.left_rotation_against_90();
-   // my_cube.left_rotation_90();
- // my_cube.front_rotation_90();
-  //my_cube.front_rotation_against_90();
-  //my_cube.back_rotation_90();
-  //my_cube.back_rotation_against_90();
-  //my_cube.up_rotation_90();
-  //my_cube.up_rotation_against_90();
-  //my_cube.down_rotation_90();
-    // my_cube.down_rotation_against_90();
-    my_cube.random_situation();
-   // my_cube.show();
+    // чтение с файла
+    my_cube.fill_from_the_file("file.txt");
+
+    // проверка
+    my_cube.check_answer_for_cout();
+
+    // вывод в консоль
     my_cube.show_to_concol();
-    cout << my_cube.check() << '\n';
+
+    // решение куба
+    my_cube.main_solver();
+
+    // вывод в файл
+    my_cube.show();
+
+    // проверка собранного куба
+    my_cube.check_solved_answer_for_cout();
+
+    // вывод в консоль
+    my_cube.show_to_concol();
     return 0;
 
 }
