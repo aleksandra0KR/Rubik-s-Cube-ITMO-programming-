fcecf46916a83f63045ddf1a8e9653c5c6e2d135
Andrey <102759385+KoshkoBlin@users.noreply.github.com>
2023-05-28 18:29:10 +0300

    Update cube.h

--- cube.h
+++ cube.h
@@ -1,12 +1,11 @@
 #pragma once
-
+#include <GL/glut.h>
 #include <vector>
 #include <iostream>
-
 using std::cout;
 using std::vector;
 
-enum colors {
+enum colors{
     GREEN,
     WHITE,
     BLUE,
@@ -15,7 +14,7 @@ enum colors {
     RED
 };
 
-class Cube {
+class Cube{
 private:
     int const size = 3;
     vector<vector<colors>> left;
@@ -24,17 +23,30 @@ private:
     vector<vector<colors>> back;
     vector<vector<colors>> up;
     vector<vector<colors>> down;
-
+    struct palette{
+        float RED;
+        float GREEN;
+        float BLUE;
+    };
+    struct x_y_z{
+        float x;
+        float y;
+        float z;
+    };
+    float rotate_x = 0;
+    float rotate_y = 0;
 public:
-    Cube() {
-        for (int i = 0; i < size; i++) {
+
+
+    Cube(){
+        for(int i = 0; i < size; i++){
             left.push_back({});
             front.push_back({});
             right.push_back({});
             back.push_back({});
             up.push_back({});
             down.push_back({});
-            for (int j = 0; j < size; j++) {
+            for(int j = 0; j < size; j++){
                 left[i].push_back(GREEN);
                 front[i].push_back(WHITE);
                 right[i].push_back(BLUE);
@@ -44,7 +56,6 @@ public:
             }
         }
     }
-
     void random_situation();
 
     void fill_from_the_file(const char *name_of_the_file);
@@ -56,7 +67,6 @@ public:
     bool has_all_colors();
 
     bool check();
-
 
     void right_rotation_90();
 
@@ -120,5 +130,15 @@ public:
 
     bool check_solved();
     void check_solved_answer_for_cout();
-    void check_answer_for_cout();
+
+    void call_coloring();
+    void coloring(vector<vector<colors>> &side, int k);
+    void square_mini(palette tmp, x_y_z a);
+    void square(float x, float y, float z, float rotate, vector<palette> tmp);
+    void display_f();
+    void specialKeys_f(int key, int x, int y);
+    void normalKeys_f(unsigned char key, int x, int y);
+
+
+
 };
