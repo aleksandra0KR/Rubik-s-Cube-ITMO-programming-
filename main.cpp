60a4c90557c8685d8dd44114d41ef1888197cf62
Andrey <102759385+KoshkoBlin@users.noreply.github.com>
2023-05-28 18:28:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,35 @@
 #include "cube.h"
-
-int main() {
-
-
-    Cube my_cube;
-
-    // чтение с файла
-    my_cube.fill_from_the_file("file.txt");
-
-    // проверка
-    my_cube.check_answer_for_cout();
-
-    // вывод в консоль
-    my_cube.show_to_concol();
-
-    // решение куба
-    my_cube.main_solver();
-
-    // проверка собранного куба
-    my_cube.check_solved_answer_for_cout();
-
-    // вывод в консоль
-    my_cube.show_to_concol();
-
-    // рандомная ситуация
-    my_cube.random_situation();
-
-    my_cube.main_solver();
-
-    // вывод в файл
-    my_cube.show();
+Cube my_cube;
 
 
 
-    return 0;
+void specialKeys(int key, int x, int y){
+    my_cube.specialKeys_f(key, x, y);
+}
+
+void normalKeys(unsigned char key, int x, int y){
+    my_cube.normalKeys_f(key, x, y);
+}
+
+void display(){
+    my_cube.display_f();
+}
+
+
+
+int main(int argc, char* argv[]){
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(500,500);
+  glutCreateWindow("Cube");
+  glEnable(GL_DEPTH_TEST);
+  glClearColor(0.2,0.2,0.2,1);
+  glutDisplayFunc(display);
+  glutKeyboardFunc(normalKeys);
+  glutSpecialFunc(specialKeys);
+  glutMainLoop();
+  return 0;
 
 }
+
+
